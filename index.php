980aa295ed8f17e148bc2a42ad5ff56f9b55ba48
Sanjeewa Liyanage <138761214+Sanjeewa-Liyanage@users.noreply.github.com>
2025-06-26 02:16:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,12 @@
 <?php
-// --- CORS Headers ---
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 
-// --- Handle Preflight Requests ---
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(204);
-    exit();
-}
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
 
 require_once 'src/utils/imports.php';
 require_once 'src/utils/router.php';
 
 session_start();
 $router = new Router();
-$router->runScript();
+$router-> runScript();
+// hi
